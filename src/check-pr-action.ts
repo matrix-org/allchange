34a0e94db02b9d47f24e29051268f6eb0c19baba
David Baker <dave@matrix.org>
2021-08-06 17:37:31 +0100

    more logging & case insensitive

--- src/check-pr-action.ts
+++ src/check-pr-action.ts
@@ -81,7 +81,8 @@ function hasLabel(label: string, pr: PrInfo): boolean {
 }
 
 async function addLabels(octokit: SortOfAnOctokit, pr: PrInfo): Promise<PrInfo> {
-    const matches = pr.body.match(/^Type: ([\w-]+)/);
+    const matches = pr.body.match(/^Type: ([\w-]+)/i);
+    console.log("matches: ", matches);
     if (matches) {
         let changeType;
         switch (matches[1].toLowerCase()) {
